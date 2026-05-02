8467df1b0e6ea22def333857a656413b246c51b0
unknown <danilim430@gmail.com>
2026-05-03 04:14:06 +0700

    tugas 5

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Admin\DokterController;
 use App\Http\Controllers\Admin\PasienController; 
 use App\Http\Controllers\Admin\ObatController;
 use App\Http\Controllers\Dokter\JadwalPeriksaController;
+use App\Http\Controllers\Dokter\PeriksaPasienController;
+// Import Controller RiwayatPasien dari foto
+use App\Http\Controllers\Dokter\RiwayatPasienController;
 // Tambahkan Import untuk Controller Pasien
 use App\Http\Controllers\Pasien\PoliController as PasienPoliController; 
 use Illuminate\Support\Facades\Route;
@@ -44,6 +47,15 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
     
     // Jadwal Periksa
     Route::resource('jadwal-periksa', JadwalPeriksaController::class);
+
+    // Periksa Pasien   
+    Route::get('/periksa-pasien', [PeriksaPasienController::class, 'index'])->name('periksa-pasien.index');
+    Route::post('/periksa-pasien', [PeriksaPasienController::class, 'store'])->name('periksa-pasien.store');
+    Route::get('/periksa-pasien/{id}', [PeriksaPasienController::class, 'create'])->name('periksa-pasien.create');
+
+    // Riwayat Pasien 
+    Route::get('/riwayat-pasien', [RiwayatPasienController::class, 'index'])->name('riwayat-pasien.index');
+    Route::get('/riwayat-pasien/{id}', [RiwayatPasienController::class, 'show'])->name('riwayat-pasien.show');
 });
 
 // ================= PASIEN =================
